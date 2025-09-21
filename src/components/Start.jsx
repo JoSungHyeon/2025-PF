5da8f718e8bbb41fc8d3d698c8ea065353c13562
JoSungHyoen <jsh950618@gmail.com>
2025-09-21 22:46:00 +0900

    +scroll

--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -1,7 +1,6 @@
 import styles from './css/Start.module.css';
 import homeData from '../homeData';
 
-
 function Start() {
 	let {mainVideo} = homeData
 
@@ -12,6 +11,22 @@ function Start() {
 				<div className={styles.mainText}>
 					<h2>SH' <span>PORTFOLIO</span></h2>
 					<p>더 나은 코드를 고민하는 퍼블리셔 & 프론트엔드 개발자</p>
+				</div>
+				<div className={styles.scrollBtn}>
+					<span>SCROLL</span>
+					  <svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="20"
+						height="20"
+						viewBox="0 0 24 24"
+						fill="none"
+						stroke="white"
+						strokeWidth="2"
+						strokeLinecap="round"
+						strokeLinejoin="round"
+					>
+						<polyline points="6 9 12 15 18 9" />
+					</svg>
 				</div>
 			</main>
 		</div>
