e1457aab4b0e10ac1fd80c2cd3c28edcaded7a73
JoSungHyoen <jsh950618@gmail.com>
2025-10-21 17:59:52 +0900

    +about text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -19,6 +19,7 @@ function About() {
 								<i class="fa-solid fa-pen-nib"></i>
 							</div>
 							<div className={styles.valueDesc}>
+								<strong>Publishing</strong>
 								<p>디자인 의도를 정확히 구현하며, 웹 표준과 접근성을 고려한 퍼블리싱을 지향합니다. 세밀한 디테일까지 신경 써 완성도 높은 결과물을 만듭니다.</p>
 							</div>
 						</div>
@@ -27,6 +28,7 @@ function About() {
 								<i class="fa-solid fa-file-code"></i>
 							</div>
 							<div className={styles.valueDesc}>
+								<strong>Frontend</strong>
 								<p>React와 JavaScript를 기반으로 유지보수가 쉽고 효율적인 UI를 설계합니다. 사용자 경험을 중심으로, 더 나은 코드를 항상 고민합니다.</p>
 							</div>
 						</div>
@@ -35,6 +37,7 @@ function About() {
 								<i class="fa-solid fa-users"></i>
 							</div>
 							<div className={styles.valueDesc}>
+								<strong>Collaboration</strong>
 								<p>좋은 결과물은 혼자가 아닌 함께할 때 완성된다고 믿습니다. 서로의 아이디어를 존중하며, 팀 전체가 성장하는 협업을 지향합니다.</p>
 							</div>
 						</div>
